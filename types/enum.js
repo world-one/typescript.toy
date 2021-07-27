da99f39852b024002917239bea26d6c8e73bb281
world-one <worldone85@gmail.com>
2021-07-27 22:11:48 +0900

    learn: enum compile and tree shaking

--- types/enum.js
+++ types/enum.js
@@ -36,3 +36,15 @@ var IndexEnumAddValue3;
     IndexEnumAddValue3[IndexEnumAddValue3["C"] = 12] = "C";
     IndexEnumAddValue3[IndexEnumAddValue3["D"] = 13] = "D";
 })(IndexEnumAddValue3 || (IndexEnumAddValue3 = {}));
+// enum tree shaking
+var selectedName = CompanyNames.APPLE;
+var selectedName2 = "kakao" /* KAKAO */;
+var selectedName3 = "naver" /* NAVER */;
+var selectedName4 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." /* LONG_NAME */;
+var COMPANY_NAMES = {
+    GOOGLE: 'google',
+    FACEBOOK: 'facebook',
+    APPLE: 'apple'
+};
+var selectedName5 = COMPANY_NAMES.FACEBOOK;
+var selectedName6 = CompanyNames.APPLE;
--- types/enum.ts
+++ types/enum.ts
@@ -36,3 +36,26 @@ enum IndexEnumAddValue3{
   C = 12,
   D
 }
+
+// enum tree shaking
+const selectedName = CompanyNames.APPLE;
+
+const enum CompanyNames2 {
+  NAVER = 'naver',
+  KAKAO = 'kakao',
+  LONG_NAME = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+}
+
+const selectedName2 = CompanyNames2.KAKAO;
+const selectedName3 = CompanyNames2.NAVER;
+const selectedName4 = CompanyNames2.LONG_NAME;
+
+const COMPANY_NAMES = {
+  GOOGLE: 'google',
+  FACEBOOK: 'facebook',
+  APPLE: 'apple',
+}
+type COMPANY_NAMES = typeof COMPANY_NAMES[keyof typeof COMPANY_NAMES];
+
+const selectedName5: COMPANY_NAMES = COMPANY_NAMES.FACEBOOK;
+const selectedName6: CompanyNames = CompanyNames.APPLE;
