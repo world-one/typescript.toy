363210eef391d219f396b7a1db8773ac7f3416fa
world-one <worldone85@gmail.com>
2021-07-28 23:12:30 +0900

    feat: create file

---
+++ createFile/createFile_1627481478148.ts
@@ -0,0 +1,4 @@
+const userInfo = {
+  "name": "world-one",
+  "age": 18
+}
---
+++ createFile/index.ts
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const date = Date.now();
+const contents = {
+  name: 'world-one',
+  age: 18,
+}
+
+fs.writeFile(`createFile/createFile_${date}.ts`, `const userInfo = ${JSON.stringify(contents, null, 2)}`, function (err) {
+  if (err) throw err;
+  console.log('File is created successfully.');
+});
