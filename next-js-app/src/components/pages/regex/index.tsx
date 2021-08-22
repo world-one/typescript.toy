2c9703e1097f97a03434f178deddf4720e9d4da1
world-one <worldone85@gmail.com>
2021-08-22 21:57:05 +0900

    feat: add highlight when matching

--- next-js-app/src/components/pages/regex/index.tsx
+++ next-js-app/src/components/pages/regex/index.tsx
@@ -1,27 +1,51 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 
 const Regex: FC = () => {
   const [machingWords, setMachingWords] = useState<RegExpMatchArray>([]);
-  const [pattern, setPattern] = useState<string>('see (chapter \d+(\.\d)*)');
-
+  // const [pattern, setPattern] = useState<string>('see (chapter \d+(\.\d)*)');
   const str = 'For more information, see Chapter 3.4.5.1'; 
-  function findMatchingWords() {
+  const [highlight, setHighlight] = useState<string>(str);
+
+  function findMatchingWords(pattern: string) {
     const regexPattern = new RegExp(pattern, 'g');
+    console.log(regexPattern);
     const match: RegExpMatchArray | null = str.match(regexPattern);
     console.log(match);
     if (!match) return;
     setMachingWords(match);
+    const highlightText = str.replace(regexPattern, `<mark>${pattern}</mark>`);
+    console.log(highlightText);
+    setHighlight(highlightText);
   }
+
+  useEffect(() => {
+    const canvas: any = document.getElementById("canvas");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+
+      ctx.fillStyle = "rgb(200,0,0)";
+      ctx.fillRect (110, 10, 50, 50);
+
+      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+      ctx.fillRect (30, 30, 50, 50);
+    }
+  },[])
 
   return (
     <>
-    <div style={{ padding: 24, }}>
-      <p>{str}</p>
-      <ul>
+    <div style={{ padding: 24, backgroundColor: '#aaa'}}>
+      <div  style={{ position: 'relative', width: 300, height: 250, border: '1px solid #000'}}>
+        <canvas id={'canvas'} width="300" height="250" style={{ position: 'absolute', top: 0 }} />
+        <div style={{ position: 'absolute', top: 0, }}>
+          <p>{str}</p>
+          <p dangerouslySetInnerHTML={{ __html: highlight }} />
+        </div>
+      </div>
+      {/* <ul>
         {machingWords.map((item, index) => <li key={index}>{item}</li>)}  
-      </ul>
-      <input value={`${pattern}`} onChange={(e) => setPattern(e.target.value)} />
-      <button onClick={() => {void findMatchingWords()}}>찾기</button>
+      </ul> */}
+      <input onChange={(e) => void findMatchingWords(e.target.value)} />
+      {/* <button onClick={() => {void findMatchingWords()}}>찾기</button> */}
     </div>
     </>
   );
