2562299c649304ab11321de7be12d6487763fe02
world-one <worldone85@gmail.com>
2021-08-23 22:51:09 +0900

    feat: add email pattern

--- next-js-app/src/components/pages/regex/index.tsx
+++ next-js-app/src/components/pages/regex/index.tsx
@@ -4,51 +4,78 @@ const Regex: FC = () => {
   const [machingWords, setMachingWords] = useState<RegExpMatchArray>([]);
   // const [pattern, setPattern] = useState<string>('see (chapter \d+(\.\d)*)');
   const str = 'For more information, see Chapter 3.4.5.1'; 
+  const email = 'worldone.dev@gmail.com';
   const [highlight, setHighlight] = useState<string>(str);
-
+  const [isMatching, setIsMaching] = useState<any>('null');
+  
   function findMatchingWords(pattern: string) {
-    const regexPattern = new RegExp(pattern, 'g');
-    console.log(regexPattern);
-    const match: RegExpMatchArray | null = str.match(regexPattern);
-    console.log(match);
-    if (!match) return;
-    setMachingWords(match);
-    const highlightText = str.replace(regexPattern, `<mark>${pattern}</mark>`);
-    console.log(highlightText);
-    setHighlight(highlightText);
+    if (pattern.length === 0) return;
+    try {
+      const regexPattern = new RegExp(pattern, 'g');  
+      console.log(regexPattern);
+      const match: RegExpMatchArray | null = email.match(regexPattern);
+      console.log(match);
+      if (!match) return;
+      setMachingWords(match);
+    } catch (error) {
+      console.log(error);
+    }
+    
+    
+    // const highlightText = str.replace(regexPattern, `<mark>${pattern}</mark>`);
+    // console.log(highlightText);
+    // setHighlight(highlightText);
   }
 
-  useEffect(() => {
-    const canvas: any = document.getElementById("canvas");
-    if (canvas.getContext) {
-      const ctx = canvas.getContext("2d");
+  // useEffect(() => {
+  //   const canvas: any = document.getElementById("canvas");
+  //   if (canvas.getContext) {
+  //     const ctx = canvas.getContext("2d");
 
-      ctx.fillStyle = "rgb(200,0,0)";
-      ctx.fillRect (110, 10, 50, 50);
+  //     ctx.fillStyle = "rgb(200,0,0)";
+  //     ctx.fillRect (110, 10, 50, 50);
 
-      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-      ctx.fillRect (30, 30, 50, 50);
-    }
-  },[])
+  //     ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+  //     ctx.fillRect (30, 30, 50, 50);
+  //   }
+  // },[])
 
   return (
     <>
     <div style={{ padding: 24, backgroundColor: '#aaa'}}>
       <div  style={{ position: 'relative', width: 300, height: 250, border: '1px solid #000'}}>
-        <canvas id={'canvas'} width="300" height="250" style={{ position: 'absolute', top: 0 }} />
+        {/* <canvas id={'canvas'} width="300" height="250" style={{ position: 'absolute', top: 0 }} /> */}
         <div style={{ position: 'absolute', top: 0, }}>
           <p>{str}</p>
-          <p dangerouslySetInnerHTML={{ __html: highlight }} />
+          <p>{email}</p>
+          {/* <p dangerouslySetInnerHTML={{ __html: highlight }} /> */}
         </div>
       </div>
-      {/* <ul>
+      <ul>
         {machingWords.map((item, index) => <li key={index}>{item}</li>)}  
-      </ul> */}
+      </ul>
       <input onChange={(e) => void findMatchingWords(e.target.value)} />
       {/* <button onClick={() => {void findMatchingWords()}}>찾기</button> */}
+    </div>
+    <div>
+      <input type="text" onChange={(e) => {
+        const value: string = e.target.value;
+        const pattern = new RegExp(PATTERN.EMAIL, 'g');
+        console.log(pattern);
+        const matchingWords = value.match(pattern);
+        console.log(machingWords);
+        
+
+        setIsMaching(`${matchingWords} [${pattern.test(value)}]`);
+      }}/>
+      <p>{isMatching}</p>
     </div>
     </>
   );
 }
 
 export default Regex;
+
+const PATTERN = {
+  EMAIL: '^[a-zA-Z0-9]+@[a-zA-Z]+(\.[a-zA-Z]{2,3})$',
+}
