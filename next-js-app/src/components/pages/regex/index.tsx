ab70122b3ab9a0572a58f15424defe59f4ceda2e
world-one <worldone85@gmail.com>
2021-08-26 21:48:03 +0900

    refact: regex page

--- next-js-app/src/components/pages/regex/index.tsx
+++ next-js-app/src/components/pages/regex/index.tsx
@@ -1,84 +1,51 @@
 import React, { FC, useState, useEffect } from 'react';
 
 const Regex: FC = () => {
-  const [matchingWords, setMatchingWords] = useState<RegExpMatchArray>([]);
-
-  // const [pattern, setPattern] = useState<string>('see (chapter \d+(\.\d)*)');
-  const str = 'For more information, see Chapter 3.4.5.1'; 
-  const email = 'worldone.dev@gmail.com';
-  const [highlight, setHighlight] = useState<string>(str);
-  const [isMatching, setIsMaching] = useState<any>('null');
+  const [matched, setMatched] = useState<any>({
+    email: null
+  });
   
-  function findMatchingWords(pattern: string) {
-    if (pattern.length === 0) return;
-    try {
-      const regexPattern = new RegExp(pattern, 'g');  
-      console.log(regexPattern);
-      const match: RegExpMatchArray | null = email.match(regexPattern);
-      console.log(match);
-      if (!match) return;
-      setMatchingWords(match);
-    } catch (error) {
-      console.log(error);
-    }
-    
-    
-    // const highlightText = str.replace(regexPattern, `<mark>${pattern}</mark>`);
-    // console.log(highlightText);
-    // setHighlight(highlightText);
+  function findMatchingWords(name: string, value: string, pattern: string): void {
+    if (!value) return;
+    const regex: RegExp = new RegExp(pattern, 'g');
+    const matched = value.match(pattern);
+    const isValid: boolean = regex.test(value);
+    setMatched((prev: any) => {
+      prev[name] = `${matched} [${isValid}]`
+      return { ...prev }
+    });
   }
 
-  // useEffect(() => {
-  //   const canvas: any = document.getElementById("canvas");
-  //   if (canvas.getContext) {
-  //     const ctx = canvas.getContext("2d");
-
-  //     ctx.fillStyle = "rgb(200,0,0)";
-  //     ctx.fillRect (110, 10, 50, 50);
-
-  //     ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-  //     ctx.fillRect (30, 30, 50, 50);
-  //   }
-  // },[])
-
   return (
-    <>
     <div style={{ padding: 24, backgroundColor: '#aaa'}}>
-      <div  style={{ position: 'relative', width: 300, height: 250, border: '1px solid #000'}}>
-        {/* <canvas id={'canvas'} width="300" height="250" style={{ position: 'absolute', top: 0 }} /> */}
-        <div style={{ position: 'absolute', top: 0, }}>
-          <p>{str}</p>
-          <p>{email}</p>
-          {/* <p dangerouslySetInnerHTML={{ __html: highlight }} /> */}
-        </div>
-      </div>
       <ul>
-        {matchingWords?.map((item, index) => <li key={index}>{item}</li>)}  
+        {
+          formItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <span style={{ paddingRight: 20, }}>{item.name}</span>
+                <input type="text" onChange={(e) => void findMatchingWords(item.name, e.target.value, item.pattern)}/>
+                <p>{matched[item.name]}</p>
+              </li>
+            )
+          })
+        }
       </ul>
-      <input onChange={(e) => void findMatchingWords(e.target.value)} />
-      {/* <button onClick={() => {void findMatchingWords()}}>찾기</button> */}
     </div>
-    <div>
-      <input type="text" onChange={(e) => {
-        const value: string = e.target.value;
-        const pattern = new RegExp(PATTERN.EMAIL_B, 'g');
-        console.log(pattern);
-        const matched = value.match(pattern);
-        console.log(matched);
-        
-
-        setIsMaching(`${matched} [${pattern.test(value)}]`);
-      }}/>
-      <p>{isMatching}</p>
-    </div>
-    </>
   );
 }
 
 export default Regex;
 
+const TEST_TEXT = {
+  DEFAULT: 'For more information, see Chapter 3.4.5.1',
+  EMAIL: 'worldone.dev@gmail.com',
+}
+
 // abcd@gggg @ 뒤쪽으로 4글자 이상이 오면 일치
 const PATTERN = {
+  NAME_KO: '[가-힣]+',
+  NUMBER: '[0-9]+',
   EMAIL_A: '^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3})$', // abcd@gggg 인 경우 일치..
   //RegExp("pattern") 표기를 쓰면서 \ 자체를 이스케이프 해줘야한다.
   EMAIL_B: '^[a-zA-Z0-9]+@\\w+\\.[a-zA-Z]{2,3}$', 
@@ -86,3 +53,17 @@ const PATTERN = {
   EMAIL_D: '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
 }
 
+const formItems = [
+  {
+    name: 'name_ko',
+    pattern: PATTERN.NAME_KO,
+  },
+  {
+    name: 'number',
+    pattern: PATTERN.NUMBER,
+  },
+  {
+    name: 'email',
+    pattern: PATTERN.EMAIL_B,
+  },
+]
