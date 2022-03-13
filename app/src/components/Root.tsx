33e0effe839e9e67ebeea1ebf9babfd681b3b6f7
world-one <worldone85@gmail.com>
2022-03-13 22:42:58 +0900

    feat: add library three

--- app/src/components/Root.tsx
+++ app/src/components/Root.tsx
@@ -1,8 +1,40 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber'
+
 
 const Root: FC = () => {
-  return <h1>hello, world</h1>
-  
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
 }
 
 export default Root;
+
+
+function Box(props: any) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref: any = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState<any>(false)
+  const [clicked, click] = useState<any>(false)
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
