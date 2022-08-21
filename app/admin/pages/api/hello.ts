1123950a88e983760e8db08b89e11ce9806c55da
world-one <worldone.dev@gmail.com>
2022-08-21 18:07:25 +0900

    chore: test notion api

--- app/admin/pages/api/hello.ts
+++ app/admin/pages/api/hello.ts
@@ -5,9 +5,28 @@ type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  const url = `https://api.notion.com/v1/databases/${process.env.DATA_ID}/query`;
+  const options = {
+    headers: {
+      Authorization: `Bearer ${process.env.API_TOKEN}`,
+      "Notion-Version": "2022-06-28"
+    },
+    method: 'POST'
+  };
+
+  try {
+   const result = await fetch(url, options).then((res) => res.json()).then((result) => result);
+    console.log(result.results[0])
+
+  } catch (e) {
+    console.error(e)
+  }
+
+
   res.status(200).json({ name: 'John Doe' })
 }
--- app/admin/src/components/pages/main/MainPage.tsx
+++ app/admin/src/components/pages/main/MainPage.tsx
@@ -1,16 +1,40 @@
-import { FC } from 'react';
+import {FC, useEffect, useState} from 'react';
 import Button from "@mui/material/Button";
-import {Box, Container, Typography} from "@mui/material";
+import {Autocomplete, Box, Container, TextField, Typography} from "@mui/material";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
 
 interface PropTypes {}
 
 const MainPage: FC<PropTypes> = () => {
+  const [value, setValue] = useState<any>();
+
+  useEffect(() => {
+
+  },[])
+
   return <div>
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js example
         </Typography>
+        <Autocomplete
+          disablePortal
+          sx={{width: '300px'}}
+          renderInput={(params) => <TextField {...params} />} options={top100Films} />
+
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DatePicker
+            label="Basic example"
+            value={value}
+            onChange={(newValue) => {
+              setValue(newValue);
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
         <Button variant={"contained"}>
           Go to the main page
         </Button>
@@ -19,4 +43,18 @@ const MainPage: FC<PropTypes> = () => {
   </div>
 };
 
-export default MainPage
+export default MainPage;
+
+const top100Films = [
+  { label: 'The Shawshank Redemption', year: 1994 },
+  { label: 'The Godfather', year: 1972 },
+  { label: 'The Godfather: Part II', year: 1974 },
+  { label: 'The Dark Knight', year: 2008 },
+  { label: '12 Angry Men', year: 1957 },
+  { label: "Schindler's List", year: 1993 },
+  { label: 'Pulp Fiction', year: 1994 },
+  {
+    label: 'The Lord of the Rings: The Return of the King',
+    year: 2003,
+  },
+]
