2f8971233b5f0540067fd7a0e7d3ce8f31a3ea36
world-one <worldone.dev@gmail.com>
2022-08-26 22:03:28 +0900

    feat: add date fns

---
+++ app/admin/src/utils/date.ts
@@ -0,0 +1,10 @@
+import { format, formatDistance } from "date-fns";
+
+export function convertDateFormat(date: string, ): string {
+  return format(new Date(date), 'yyyy-MM-dd');
+}
+
+function calcDateDistance(from: Date, to: Date): string{
+  return formatDistance(from, to);
+}
+
