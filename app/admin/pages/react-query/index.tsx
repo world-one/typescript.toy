c545f2bdc9f31371da9e316707f05798477f0643
world-one <worldone.dev@gmail.com>
2022-09-01 22:19:43 +0900

    feat: add react query

---
+++ app/admin/pages/react-query/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import ReactQueryPage from "../../src/components/pages/reactQuery/ReactQuery";
+
+const reactQuery: NextPage = () => {
+  return (
+    <ReactQueryPage />
+  )
+}
+
+export default reactQuery
