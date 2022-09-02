e9215d2a0c93ef2137264776982e08006f21cd10
world-one <worldone.dev@gmail.com>
2022-09-02 23:31:10 +0900

    chore: test react datepicker

--- app/admin/pages/react-query/index.tsx
+++ app/admin/pages/react-query/index.tsx
@@ -1,10 +1,38 @@
 import type { NextPage } from 'next'
 import ReactQueryPage from "../../src/components/pages/reactQuery/ReactQuery";
+import ReactDatePicker from "react-datepicker";
+import {useState} from "react";
+import "react-datepicker/dist/react-datepicker.css";
+import styled from "@emotion/styled";
 
 const reactQuery: NextPage = () => {
+  const [startDate, setStartDate] = useState(new Date());
+
   return (
-    <ReactQueryPage />
+    <div>
+      <DatePickerWrap>
+        <ReactDatePicker  selected={startDate} onChange={() => console.log('a')} />
+      </DatePickerWrap>
+      <ReactQueryPage />
+    </div>
+
   )
 }
 
-export default reactQuery
+export default reactQuery;
+
+const DatePickerWrap = styled.div`
+  margin-bottom: 160px; 
+  .react-datepicker__month-container {
+    border: 3px solid red;
+    border-radius: 16px;
+    overflow: hidden;
+  }
+  .react-datepicker__header {
+    background-color: #222222;
+  }
+  .react-datepicker__current-month {
+    color: #fff;
+  }
+`
+
