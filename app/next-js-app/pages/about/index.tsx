575054b097e8102535357e22cdcbd8de410f34db
world-one <worldone.dev@gmail.com>
2022-09-03 11:56:35 +0900

    chore: Clean up unnecessary code

--- app/next-js-app/pages/about/index.tsx
+++
@@ -1,3 +0,0 @@
-import About from '../../src/components/pages/about';
-
-export default About;
---
+++ app/next/src/components/pages/calculator/CalculatorPage.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import DrawKeyPad from './library/DrawKeypad';
+import Calc from './library/Calculator';
+
+const drawKeyPad = new DrawKeyPad(null);
+
+function Calculator() {
+  useEffect(() => {
+    void drawKeyPad.draw();
+    new Calc({
+      targetElId: 'js-keypad',
+      processElId: 'js-process',
+      resultElId: 'js-result',
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="wrap__calculator">
+        <textarea
+          className="input__data input__data__process"
+          id="js-process"
+          placeholder="0"
+          readOnly
+        ></textarea>
+        <input
+          className="input__data input__data__result"
+          id="js-result"
+          type="text"
+          placeholder="0"
+          readOnly
+        />
+        <div className="wrap__keypad" id="js-keypad">
+          <div className="keypad" id="js-keypad-btns"></div>
+          <div className="keypad operator" id="js-keypad-operators"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Calculator;
