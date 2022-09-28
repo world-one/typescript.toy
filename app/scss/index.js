e67b12255faf8a05589c6db1cd86c1f23a955d27
world-one <worldone.dev@gmail.com>
2022-09-28 22:23:19 +0900

    test: scss

---
+++ app/scss/index.js
@@ -0,0 +1,21 @@
+import './style.scss';
+
+const NAMES = ['w', 'j', 'lee', 'a'];;
+
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+<ul class='names'>
+  <li>a</li>
+  <li>b</li>
+  <li>c</li>
+  <li>d</li>
+  <li>e</li>
+  <li>f</li>
+</ul>
+`;
+
