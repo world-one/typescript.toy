f9aefc2daeb21943f9e6e02fb6ae12297a3e80ba
world-one <worldone.dev@gmail.com>
2022-10-02 23:44:55 +0900

    feat: test react query stale

--- app/next/src/components/pages/reactQuery/ReactQuery.tsx
+++ app/next/src/components/pages/reactQuery/ReactQuery.tsx
@@ -1,16 +1,20 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import {
   QueryClient,
   useQuery,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import Button from '@mui/material/Button';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 const ReactQuery: FC = () => {
+  console.log('fe');
   return (
     <QueryClientProvider client={queryClient}>
       <MyData />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
@@ -18,14 +22,31 @@ const ReactQuery: FC = () => {
 export default ReactQuery;
 
 const MyData = () => {
-  const { isLoading, error, data } = useQuery(['data'], async () => {
-    const result = await fetch(
-      'https://api.github.com/repos/tannerlinsley/react-query'
-    );
-    return result.json();
-  });
+  const [key, setKey] = useState('red');
+  const { isLoading, error, data } = useQuery(
+    ['data', key],
+    async () => {
+      const result = await fetch(
+        'https://api.github.com/repos/tannerlinsley/react-query'
+      );
+      return result.json();
+    },
+    {
+      staleTime: 6000,
+    }
+  );
 
   console.log(isLoading, error, data);
+
   if (isLoading) return <div>is loading</div>;
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <>
+      <div>
+        <span> {key}</span>
+      </div>
+      <Button onClick={() => setKey('red')}>red</Button>
+      <Button onClick={() => setKey('blue')}>blue</Button>
+      <div>{JSON.stringify(data)}</div>
+    </>
+  );
 };
